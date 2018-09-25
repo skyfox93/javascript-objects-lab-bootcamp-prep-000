cb5f00d8d9cb6984d87e31cb957059a6a28555cb
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-25 05:11:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function updateObjectWithKeyAndValue(object,key,value){ return  Object.assign({},object,{[key]:value});}
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){ Object.assign(object,{[key]: value}); return object;}
 var recipes={};
-function deleteFromObjectByKey(object,key){var copy=Object.assign({}, object);delete copy[key];}
-function destructivelyDeleteFromObjectByKey(object,key){delete object[key];}
+function deleteFromObjectByKey(key,object){var copy=Object.assign({}, object);delete copy[key];}
+function destructivelyDeleteFromObjectByKey(key,object){delete object[key];}
