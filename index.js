eb4556d7eb199340f9389a6e71a36d4884ac7eaa
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-25 05:05:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function updateObjectWithKeyAndValue(object,key,value){ return  Object.assign({},object,{[key]:value});}
-function destructivelyUpdateObjectWithKeyAndValue(object,key,value){ Object.assign(object,{[key]: value}); return Object;}
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){ Object.assign(object,{[key]: value}); return object;}
 var recipes={};
